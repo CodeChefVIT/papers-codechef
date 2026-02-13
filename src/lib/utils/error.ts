413aee971d0a1faa02fd51ac259a3856d7a30bf5
Gslmao <seriouscubist@gmail.com>
2026-02-14 02:44:49 +0530

    seperate service logic from controller logic for paper-by-id api

---
+++ src/lib/utils/error.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+
+export class CustomError extends Error {
+    status: number;
+    
+    constructor(message: string, status: number) {
+        super(message);
+        this.status = status;
+        Object.setPrototypeOf(this, CustomError.prototype);
+    }
+}
+
+export function customErrorHandler(error: unknown, defaultMessage: string) {
+    if (error instanceof CustomError) {
+        return NextResponse.json(
+            {message: error.message},
+            {status: error.status}
+        );
+    } else {
+        return NextResponse.json(
+            {message: defaultMessage, error},
+            {status: 500}
+        );
+    }
+}
