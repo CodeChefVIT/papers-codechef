2c58532dbf668bba0000e2fc0f3ed1c57e72b052
abhitrueprogrammer <abhiaruvs3@gmail.com>
2026-02-14 22:09:03 +0530

    fix: non-nullable-type-assertion-style

--- src/app/api/papers/route.ts
+++ src/app/api/papers/route.ts
@@ -17,15 +17,6 @@ export async function GET(req: NextRequest) {
 
     return NextResponse.json(paper, { status: 200 });
   } catch (error) {
-    if (error instanceof Error) {
-      return NextResponse.json(
-        { message: "Failed to fetch papers", error: error.message },
-        {
-          status:
-            error.message === "Subject query parameter is required" ? 400 : 500,
-        },
-      );
-    }
     return NextResponse.json(
       { message: "Failed to fetch papers", error },
       { status: 500 },
