38d99d8d06ecfba01cb891c26c8f1e8f4fe3be97
Gslmao <seriouscubist@gmail.com>
2026-02-15 02:53:59 +0530

    seperated logic for course-list endpoint

---
+++ src/lib/services/subject.ts
@@ -0,0 +1,7 @@
+import { connectToDatabase } from "@/lib/database/mongoose";
+import { Course } from "@/db/course";
+
+export async function getCourseList(){
+    await connectToDatabase();
+    return await Course.find().lean();
+}
