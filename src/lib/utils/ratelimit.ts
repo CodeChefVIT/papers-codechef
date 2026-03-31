168895b7528a799ff761e9481ee2f67aa9b68df4
Yogesh <yogeshkumar.n1234@gmail.com>
2026-03-31 19:37:35 +0530

    refactor: Remove KV dependancy, migrate to upstash

--- src/lib/utils/ratelimit.ts
+++ src/lib/utils/ratelimit.ts
@@ -1,20 +1,20 @@
 import { Ratelimit } from "@upstash/ratelimit";
-import { kv } from "@vercel/kv";
+import { redis } from "./redis";
 
 // Rate limiter for AI Upload (5 requests per 15 minutes)
 export const aiUploadRatelimit = new Ratelimit({
-  redis: kv,
+  redis: redis,
   limiter: Ratelimit.slidingWindow(5, "900 s"),
 });
 
 // Rate limiter for Paper Requests (5 requests per 15 minutes)
 export const paperRequestRatelimit = new Ratelimit({
-  redis: kv,
+  redis: redis,
   limiter: Ratelimit.slidingWindow(5, "900 s"),
 });
 
 // Rate limiter for Subscriptions (3 requests per hour)
 export const subscribeRatelimit = new Ratelimit({
-  redis: kv,
+  redis: redis,
   limiter: Ratelimit.slidingWindow(3, "1 h"),
 });
