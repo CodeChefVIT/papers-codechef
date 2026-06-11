fe4ee845a8d5ea88ce6bd3f8f964358f41cdee30
Gslmao <seriouscubist@gmail.com>
2026-06-12 00:16:44 +0530

    update frontend callers to match api shape

--- src/app/actions/get-papers-by-id.ts
+++ src/app/actions/get-papers-by-id.ts
@@ -1,20 +1,23 @@
-import { type PaperResponse } from "@/interface";
+import { type PaperResponse, ApiResponse } from "@/interface";
 import axios, { type AxiosResponse } from "axios";
 
 export const fetchPaperID = async (id: string): Promise<PaperResponse> => {
   const serverUrl = process.env.SERVER_URL ?? "https://papers.codechefvit.com";
 
   try {
-    const response: AxiosResponse<PaperResponse> = await axios.get(
+    const response: AxiosResponse<ApiResponse<PaperResponse>> = await axios.get(
       `${serverUrl}/api/paper-by-id/${id}`,
     );
-    return response.data;
+
+    if (!response.data.data) {
+      throw new Error("Paper not found");
+    }
+    return response.data.data;
   } catch (err: unknown) {
     if (axios.isAxiosError(err)) {
-      console.error("Axios error:", err.response?.data ?? err.message);
+      console.error("Axios error:", err.response?.data.message ?? err.message);
       const errorMessage =
-        (err.response?.data as { message?: string })?.message ??
-        "Failed to fetch paper";
+        (err.response?.data.message ?? "Failed to fetch paper");
       throw new Error(errorMessage);
     } else {
       console.error("Unexpected error:", err);
