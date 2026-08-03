10a8eead2240e0d737fff4765754be2bd3f56c53
Yogesh <yogeshkumar.n1234@gmail.com>
2026-08-03 12:24:21 +0530

    fix: edit pageNumber

--- src/components/newPdfViewer.tsx
+++ src/components/newPdfViewer.tsx
@@ -124,9 +124,20 @@ const pageChange = useCallback(
         <input
           type="text"
           value={pageNo}
-          onChange={(e) => setPageNo(e.target.value)}
+          onChange={(e) => {
+            editingRef.current = true;
+            setPageNo(e.target.value.replace(/[^0-9]/g, ""));
+          }}
           onKeyDown={pageChange}
-          onFocus={() => setPageNo("")}
+          onFocus={(e) => {
+            editingRef.current = true;
+            e.target.select();
+          }}
+          onBlur={() => {
+            editingRef.current = false;
+            setPageNo(String(scrollProv?.getCurrentPage() ?? 1));
+          }}
+          inputMode="numeric"
           className="h-9 w-14 rounded border bg-[#e7e9ff] p-1 text-center text-sm [appearance:textfield] dark:bg-[#1f1f2a] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none"
         />
         <span className="text-xs font-medium text-white">{totalPages > 0 ? `of ${totalPages}` : "loading…"}</span>
